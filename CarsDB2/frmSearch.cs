7d63a4b0c02e056486b19aa6fd796fa6b8c2b6af
John Murphy <murphy.john1991@gmail.com>
2017-11-13 13:48:51 +0000

    Run method added to Search
    Accidentally left out of the previous commit.

--- CarsDB2/frmSearch.cs
+++ CarsDB2/frmSearch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,57 @@ namespace CarsDB2
 {
     public partial class frmSearch : Form
     {
+        String strConnect = @"Data Source=JOHN\SQLEXPRESS;Initial Catalog=Hire;Integrated Security=True;";
+        DataSet dataSet = new DataSet();
+
         public frmSearch()
         {
             InitializeComponent();
             this.Text = "Task A Search - John Murphy - " + DateTime.Now.ToShortDateString();
+
+            //populate cboField
+            cboField.Items.Add("Make");
+            cboField.Items.Add("EngineSize");
+            cboField.Items.Add("RentalPerDay");
+            cboField.Items.Add("Available");
+
+            //populate cboOperator
+            cboOperator.Items.Add(" = ");
+            cboOperator.Items.Add(" > ");
+            cboOperator.Items.Add(" >= ");
+            cboOperator.Items.Add(" < ");
+            cboOperator.Items.Add(" <= ");
         }
 
         private void btnRun_Click(object sender, EventArgs e)
         {
+            dataSet.Reset();
+            string strField = cboField.SelectedItem.ToString();
+            string strOperator = cboOperator.SelectedItem.ToString();
+            string strValue = txtValue.Text.Trim();
+            string strSearch = "" + strField + " " + strOperator + " " + "'"+strValue+"'";
 
+            try
+            {
+                using (SqlConnection sqlConn = new SqlConnection(strConnect))
+                {
+                    sqlConn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = sqlConn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM tblCar WHERE "+@strSearch;
+
+                    SqlDataAdapter searchAdapter = new SqlDataAdapter(cmd);
+                    searchAdapter.Fill(dataSet);
+                    dgvSearchResults.DataSource = dataSet.Tables[0];
+                    sqlConn.Close();
+
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -32,6 +75,7 @@ namespace CarsDB2
         {
             // TODO: This line of code loads data into the 'hireDataSet.tblCar' table. You can move, or remove it, as needed.
             this.tblCarTableAdapter.Fill(this.hireDataSet.tblCar);
+            
 
         }
     }
